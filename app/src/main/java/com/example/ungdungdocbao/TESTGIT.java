e9a6b61af57a8abba827f9679fa6492c94b9a69d
mtiendat <73454933+mtiendat@users.noreply.github.com>
2020-11-19 22:43:02 +0700

    TESTGIT

---
+++ app/src/main/java/com/example/ungdungdocbao/TESTGIT.java
@@ -0,0 +1,10 @@
+package com.example.ungdungdocbao;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+
+public class TESTGIT extends BottomNavPagerAdapter {
+    public TESTGIT(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+}
