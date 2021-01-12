347a4c45bf3fb51b29c44f8bef90365c79bcfc29
mtiendat <73454933+mtiendat@users.noreply.github.com>
2021-01-12 10:08:37 +0700

    đăng nhập google

--- app/src/main/java/com/example/ungdungdocbao/Activity/DangNhap.java
+++ app/src/main/java/com/example/ungdungdocbao/Activity/DangNhap.java
@@ -46,6 +46,14 @@ import com.example.ungdungdocbao.Bottom_Nav.setting.SettingFragment;
 import com.android.volley.Request;
 import com.example.ungdungdocbao.R;
 import com.facebook.FacebookCallback;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -60,11 +68,13 @@ public class DangNhap extends AppCompatActivity {
     //TextView txtEmail, txtPass;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private GoogleSignInClient mGoogleSignInClient;
     private EditText txtEmail, txtPass;
     private TextView txtQuenPass;
     private Button btnDangNhap;
     String name,image,email,id;
     private static String URL_DANGNHAP = "http://10.0.2.2:8000/api/dang-nhap";
+    private final int RC_SIGN_IN = 999;
     SaveState saveState;
 
 
@@ -80,87 +90,30 @@ public class DangNhap extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-        setContentView(R.layout.dang_nhap);
-        loginButton=findViewById(R.id.login_button);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        loginButton.setBackgroundResource(R.drawable.custom_button);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-
-                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                       id = null;
-                        try {
-                            id = object.getString("id");
-
-                        try {
-
-                            //URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
-                            //Log.i("profile_pic", profile_pic + "");
-                            //email = response.getJSONObject().getString("email");
-                            String first_name = object.getString("first_name");
-                            String last_name = object.getString("last_name");
-                            name= last_name+" "+first_name;
-                            image= object.getJSONObject("picture").getJSONObject("data").getString("url");
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id, first_name, last_name, email,gender,picture.type(large)");
-                request.setParameters(parameters);
-                request.executeAsync();
-                Toast.makeText(DangNhap.this,"Đăng nhập facebook thành công", Toast.LENGTH_LONG).show();
-                Thread thread = new Thread(){
-                    @Override
-                    public void run() {
-                        try {
-                            Thread.sleep(3000); // Set time LENGTH_LONG Toast
-                            FirstFragment.USER_NAME=name;
-                            FirstFragment.USER_EMAIL="";
-                            FirstFragment.USER_AVATAR=image;
-                            FirstFragment.USER_ID=id;
-                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                            startActivity(intent);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                thread.start();
-            }
-            @Override
-            public void onCancel() {
-                Toast.makeText(DangNhap.this,"Login attempt canceled.", Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void onError(FacebookException e) {
-                Toast.makeText(DangNhap.this,"Login attempt failed.", Toast.LENGTH_LONG).show();
-            }
-        });
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.dang_nhap);
 
-
-
-
-        txtEmail = findViewById(R.id.editTextTextPersonName);
-        txtPass = findViewById(R.id.editTextTextPassword);
-        btnDangNhap = findViewById(R.id.btn_dangnhap2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_dangnhap);
         setSupportActionBar(toolbar);
-        ; //sudung toolbar nhu actionbar
+        //su dung toolbar nhu actionbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //set nut back cho toolbar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -170,16 +123,10 @@ public class DangNhap extends AppCompatActivity {
             }
         });
 
-
-        txtQuenPass=findViewById(R.id.txt_quenmk);
-        txtQuenPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DangNhap.this, QuenMatKhau.class);
-                startActivity(intent);
-            }
-        });
-
+        saveState = new SaveState(this);
+        txtEmail = findViewById(R.id.editTextTextPersonName);
+        txtPass = findViewById(R.id.editTextTextPassword);
+        btnDangNhap = findViewById(R.id.btn_dangnhap2);
         btnDangNhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -193,16 +140,26 @@ public class DangNhap extends AppCompatActivity {
                 }
             }
         });
+        txtQuenPass=findViewById(R.id.txt_quenmk);
+        txtQuenPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DangNhap.this, QuenMatKhau.class);
+                startActivity(intent);
+            }
+        });
 
+        NenToi(saveState);
+        LoginGoogle();
+        LoginFacebook();
 
-
-        saveState = new SaveState(this);
+    }
+    public void NenToi(SaveState saveState){
         if(saveState.getState()==true)
             getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         else
             getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
-
     public void Login(final String email, final String password) {
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DANGNHAP,
@@ -263,5 +220,103 @@ public class DangNhap extends AppCompatActivity {
 
         };
         queue.add(stringRequest);
+    }
+    public void LoginGoogle(){
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        SignInButton signInButton = findViewById (R.id.sign_in_button );
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult ( signInIntent , RC_SIGN_IN );
+            }
+        });
+    }
+    public void LoginFacebook(){
+        callbackManager = CallbackManager.Factory.create();
+        loginButton=findViewById(R.id.login_button);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        id = null;
+                        try {
+                            id = object.getString("id");
+
+                            try {
+
+
+                                String first_name = object.getString("first_name");
+                                String last_name = object.getString("last_name");
+                                name= last_name+" "+first_name;
+                                image= object.getJSONObject("picture").getJSONObject("data").getString("url");
+                                updateUI("facebook",id,name,image,null);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, first_name, last_name, email,gender,picture.type(large)");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+            }
+            @Override
+            public void onCancel() {
+                Toast.makeText(DangNhap.this,"Thất Bại.", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(DangNhap.this,"Thất Bại", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+
+            URL pic = new URL(account.getPhotoUrl().toString());
+            updateUI("google",account.getId(),account.getDisplayName(),pic.toString(),account.getEmail());
+
+
+        } catch (ApiException | MalformedURLException e) {
+
+        }
+    }
+    public void updateUI(String flatform,String UserID, String Username,String Useravatar,  String Useremail){
+        FirstFragment.USER_NAME=Username;
+        FirstFragment.USER_EMAIL=Useremail;
+        FirstFragment.USER_AVATAR=Useravatar;
+        FirstFragment.USER_ID=UserID;
+        Toast.makeText(DangNhap.this,"Đăng nhập "+flatform +" thành công", Toast.LENGTH_LONG).show();
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000); // Set time LENGTH_LONG Toast
+
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 }
